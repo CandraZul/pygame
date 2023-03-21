d0b689f782becb7bc819fa3e621bb785966cf933
CandraZul <125719042+CandraZul@users.noreply.github.com>
2023-03-21 14:03:25 +0700

    animation

--- pygame/basic.py
+++ pygame/basic.py
@@ -7,10 +7,11 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('pygame/font/Pixeltype.ttf', 50)
 
-bg_surface = pygame.image.load('pygame/graphics/Sky.png')
-surface2 = pygame.image.load('pygame/graphics/ground.png')
+bg_surface = pygame.image.load('pygame/graphics/Sky.png').convert()
+surface2 = pygame.image.load('pygame/graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black') #format ('kata yang ditampilkan', huruf mulus atau tidak, warna)
-
+snail = pygame.image.load('pygame/graphics/snail/snail1.png').convert_alpha()
+snail_x_position = 600
 
 while True:
     for event in pygame.event.get():
@@ -18,10 +19,16 @@ while True:
             pygame.quit()
             exit()
        
-        screen.blit(bg_surface, (0,0))
-        screen.blit(surface2, (0,300))
-        screen.blit(text_surface, (350, 50))
-        
+    screen.blit(bg_surface, (0,0))
+    screen.blit(surface2, (0,300))
+    screen.blit(text_surface, (350, 50))
+    screen.blit(snail, (snail_x_position, 250))
+    snail_x_position -= 3
 
-        pygame.display.update()
-        clock.tick(60)
+    if snail_x_position <= -50:
+        snail_x_position = 800
+
+    
+
+    pygame.display.update()
+    clock.tick(60)
