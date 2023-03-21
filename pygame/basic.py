ca55df67cd183ac6f586d7ed565f2403b812e041
CandraZul <125719042+CandraZul@users.noreply.github.com>
2023-03-21 14:28:19 +0700

    menambahkan player dan rectangle
    menggunakan rectangle untuk memposisikan character dengan lebih baik

--- pygame/basic.py
+++ pygame/basic.py
@@ -7,11 +7,17 @@ pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('pygame/font/Pixeltype.ttf', 50)
 
+
 bg_surface = pygame.image.load('pygame/graphics/Sky.png').convert()
 surface2 = pygame.image.load('pygame/graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'Black') #format ('kata yang ditampilkan', huruf mulus atau tidak, warna)
+
 snail = pygame.image.load('pygame/graphics/snail/snail1.png').convert_alpha()
 snail_x_position = 600
+snail_rect = snail.get_rect(midbottom = (snail_x_position, 300))
+
+player_surf = pygame.image.load('pygame/graphics/Player/player_walk_1.png').convert_alpha()
+player_rect = player_surf.get_rect(midbottom = (80,300))
 
 while True:
     for event in pygame.event.get():
@@ -22,11 +28,13 @@ while True:
     screen.blit(bg_surface, (0,0))
     screen.blit(surface2, (0,300))
     screen.blit(text_surface, (350, 50))
-    screen.blit(snail, (snail_x_position, 250))
-    snail_x_position -= 3
+    screen.blit(snail, snail_rect)
+    snail_rect.left -= 3
 
-    if snail_x_position <= -50:
-        snail_x_position = 800
+    if snail_rect.left <= -50:
+        snail_rect.left = 800
+
+    screen.blit(player_surf, player_rect)
 
     
 
